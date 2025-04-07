54be6129cc45afb2fb21478f87320a0e2c407528
Beatris <beatrisraj7@gmail.com>
2025-04-07 17:06:42 +0300

    Auth controller register

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import userController from "./src/controllers/usersController.js";
+import authController from "./src/controllers/authController.js";
 
 const router = Router()
 
 router.use('/', userController)
+router.use('/auth', authController)
+
 
 
 export default router;
