a7ab65da64a3c3d9eba90cc132d9fa7c41a5bcea
Beatris <beatrisraj7@gmail.com>
2025-04-07 17:09:49 +0300

    Auth controller login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,4 +6,8 @@ authController.get('/register', (req, res) => {
     res.render('register')
 })
 
+authController.get('/login', (req, res) => {
+    res.render('login')
+})
+
 export default authController;
