b5540390bbb8abff5ce9d8d977438602aa1953ad
Beatris <beatrisraj7@gmail.com>
2025-04-16 19:01:03 +0300

    Admin edit and delete

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -5,14 +5,19 @@ const userController = Router()
 
 userController.get('/', async (req, res) => {
     const reqestPage = parseInt(req.query.page)
-    
+    const isAdmin = req.user?.role === 'admin';    
     
     const [usesrList, limit, page] = await usersService.geAll(reqestPage)
     const total = usesrList.total;
     const totalPages = [...Array(Math.ceil(total / limit)).keys()].map(i => i + 1);
     
 
-    res.render('home', {usesrList: usesrList['users'], total, currentPage:page, totalPages})
+    res.render('home', {
+        usesrList: usesrList['users'], 
+        total, 
+        currentPage:page, 
+        totalPages, 
+        isAdmin})
 })
 
 
